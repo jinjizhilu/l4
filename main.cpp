1243843771a894463dc0ad610cf1d77db396361f
jinjizhilu <jin_sho_uke@163.com>
2018-01-17 14:18:28 +0800

    1.implement syntax tree

--- main.cpp
+++ main.cpp
@@ -7,7 +7,23 @@ using namespace std;
 
 const char* keys[] = {"+", "-", "*", "/", "define", "lambda", "if", "cons", "car", "cdr"};
 
-void LexAnalyzer::Run()
+void LexComponent::Print()
+{
+	printf("[%.3s] ", &"INT,NUM,STR,KEY"[type * 4]);
+
+	switch(type)
+	{
+	case KEY:
+	case STR:
+		cout << token << ";" << endl; break;
+	case INT:
+		cout << value << ";" << endl; break;
+	case NUM:
+		cout << fvalue << ";" << endl; break;
+	}
+}
+
+void LexAnalyzer::Parse()
 {
 	int line = 0, value;
 	char token;
@@ -33,6 +49,7 @@ void LexAnalyzer::Run()
 			LexComponent item;
 			item.type = LexComponent::KEY;
 			item.token = token;
+			item.line = line;
 			mComponents.push_back(item);
 		}
 		else if ((token == '-' && fin.peek() >= '0' && fin.peek() <= '9')
@@ -55,6 +72,7 @@ void LexAnalyzer::Run()
 			}
 			item.type = LexComponent::INT;
 			item.value = value;
+			item.line = line;
 
 			if (fin.peek() == '.')
 			{
@@ -67,7 +85,7 @@ void LexAnalyzer::Run()
 					fvalue += (token - '0') * base;
 					base /= 10;
 				}
-				item.type = LexComponent::FLOAT;
+				item.type = LexComponent::NUM;
 				item.fvalue = fvalue;
 			}
 
@@ -83,6 +101,7 @@ void LexAnalyzer::Run()
 			LexComponent item;
 			item.type = LexComponent::STR;
 			item.token = token;
+			item.line = line;
 
 			while (fin.peek() > 32 && fin.peek() < 127 && fin.peek() != ')')
 			{
@@ -100,20 +119,112 @@ void LexAnalyzer::Run()
 
 void LexAnalyzer::Print()
 {
+	cout << "========================================" << endl;
+	cout << "lex analyze result:" << endl;
 	for (auto it=mComponents.begin(); it != mComponents.end(); ++it)
 	{
-		printf("%.3s ", &"INT,FLT,STR,KEY"[it->type * 4]);
+		it->Print();	
+	}
+	cout << "========================================" << endl;
+}
 
-		switch(it->type)
+void SyntaxAnalyzer::MakeTree(const list<LexComponent> &componets)
+{
+	root = new SyntaxComponent;
+	root->count = 0;
+	root->children = new list<SyntaxComponent*>;
+	TreeIterator cur = componets.cbegin(), end = componets.cend();
+
+	while (cur != end)
+	{
+		root->children->push_back(MakeTreeRecursive(cur, end));
+		root->count += 1;
+	}
+}
+
+SyntaxComponent* SyntaxAnalyzer::MakeTreeRecursive(TreeIterator &cur, TreeIterator end)
+{
+	SyntaxComponent *node = new SyntaxComponent;
+	node->count = 0;
+
+	if (cur->token == "(")
+	{
+		++cur;
+		node->children = new list<SyntaxComponent*>;
+
+		while (cur != end && cur->token != ")")
 		{
-		case LexComponent::KEY:
-		case LexComponent::STR:
-			cout << it->token << ";" << endl; break;
-		case LexComponent::INT:
-			cout << it->value << ";" << endl; break;
-		case LexComponent::FLOAT:
-			cout << it->fvalue << ";" << endl; break;
+			node->children->push_back(MakeTreeRecursive(cur, end));
+			node->count += 1;
 		}
+
+		if (cur->token != ")")
+		{
+			printf("unmatched parenthesis in line %d.\n", cur->line);
+		}
+
+		++cur;
+	}
+	else
+	{
+		node->data = new LexComponent;
+		*(node->data) = *cur;
+		++cur;
+	}
+	return node;
+}
+
+void SyntaxAnalyzer::ClearTree()
+{
+	ClearTreeTreeRecursive(root);
+	delete root;
+	root = NULL;
+}
+
+void SyntaxAnalyzer::ClearTreeTreeRecursive(SyntaxComponent *node)
+{
+	if (node->count == 0)
+	{
+		delete node->data;
+		node->data = NULL;
+	}
+	else
+	{
+		for (auto it=node->children->begin(); it != node->children->end(); ++it)
+		{
+			ClearTreeTreeRecursive(*it);
+		}
+		delete node->children;
+		node->children = NULL;
+	}
+}
+
+void SyntaxAnalyzer::PrintTree()
+{
+	cout << "========================================" << endl;
+	cout << "syntax tree:" << endl;
+	if (root != NULL)
+	{
+		PrintTreeRecursive(root, "");
+	}
+	cout << "========================================" << endl;
+}
+
+void SyntaxAnalyzer::PrintTreeRecursive(SyntaxComponent *node, string prefix)
+{
+	if (node->count == 0)
+	{
+		cout << prefix;
+		node->data->Print();
+	}
+	else
+	{
+		cout << prefix << "(" << endl;
+		for (auto it=node->children->begin(); it != node->children->end(); ++it)
+		{
+			PrintTreeRecursive(*it, prefix + "  ");
+		}
+		cout << prefix << ")" << endl;
 	}
 }
 
@@ -134,8 +245,14 @@ int main(int argc, char **argv)
 	}
 
 	lex.SetSourceFile(src_file);
-	lex.Run();
+	lex.Parse();
 	lex.Print();
+
+	SyntaxAnalyzer syntax;
+	syntax.MakeTree(lex.GetComponents());
+	syntax.PrintTree();
+
+	syntax.ClearTree();
 
 	system("pause");
 	return 0;
--- parser.h
+++ parser.h
@@ -2,17 +2,21 @@
 
 #include <string>
 #include <vector>
+#include <list>
 #include <set>
 using namespace std;
 
 struct LexComponent
 {
-	enum {INT, FLOAT, STR, KEY};
+	enum {INT, NUM, STR, KEY};
 
 	int type;
 	string token;
 	int value;
 	float fvalue;
+	int line;
+
+	void Print();
 };
 
 class LexAnalyzer
@@ -20,11 +24,36 @@ class LexAnalyzer
 public:
 	void SetSourceFile(const string &file) {mFileName = file;}
 	void AddKey(const string &key) {mKeys.insert(key);}
-	void Run();
+	void Parse();
 	void Print();
+	list<LexComponent>& GetComponents() {return mComponents;}
 
 private:
 	string mFileName;
-	vector<LexComponent> mComponents;
+	list<LexComponent> mComponents;
 	set<string> mKeys;
+};
+
+struct SyntaxComponent
+{
+	int count;
+	union
+	{
+		LexComponent *data;
+		list<SyntaxComponent*> *children;
+	};
+};
+
+class SyntaxAnalyzer
+{
+public:
+	void MakeTree(const list<LexComponent> &components);
+	void ClearTree();
+	void PrintTree();
+private:
+	typedef list<LexComponent>::const_iterator TreeIterator;
+	SyntaxComponent* MakeTreeRecursive(TreeIterator &cur, TreeIterator end);
+	void ClearTreeTreeRecursive(SyntaxComponent *node);
+	void PrintTreeRecursive(SyntaxComponent *node, string prefix);
+	SyntaxComponent *root;
 };
